8e7ae16a0be064efd23fa2421ad6ae2e7f511ec1
Wishwa Dilshan <wishwadilshan628@gmail.com>
2025-10-01 14:16:58 +0530

    Fix navigation controller setup

--- app/src/main/java/com/yourcompany/wellnessjourney/MainActivity.kt
+++ app/src/main/java/com/yourcompany/wellnessjourney/MainActivity.kt
@@ -1,11 +1,10 @@
-package com.yourcompany.wellnessjourney // <<< YOUR CORRECT PACKAGE NAME
+package com.yourcompany.wellnessjourney
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.yourcompany.wellnessjourney.R // <<< YOUR CORRECT R FILE IMPORT
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,12 +12,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as? NavHostFragment
+            ?: throw IllegalStateException("NavHostFragment not found")
+        val navController = navHostFragment.navController
+
         val navView: BottomNavigationView = findViewById(R.id.bottom_nav_view)
-
-        // The error indicates this line: MainActivity.kt:20
-        // It means 'nav_host_fragment' is not yet inflated or configured properly
-        val navController = findNavController(R.id.nav_host_fragment)
-
         navView.setupWithNavController(navController)
     }
 }
